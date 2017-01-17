9a911db32a0eab5850f3621b3bba0a0399c42c44
Alexander Iansiti <aiansiti@outlook.com>
2017-01-17 14:45:59 -0500

    i'm normal now

--- clusters_last_stand.py
+++ clusters_last_stand.py
@@ -1,10 +1,13 @@
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import normalize
 import json
 import numpy as np
 from datetime import datetime
 startTime = datetime.now()
 vecs = json.load(open("join_freq_vecs.json"))
-means = KMeans(3)
+normalize(vecs, copy=False, axis=0)
+print "normalized"
+means = KMeans(2)
 means.fit(vecs)
-np.savetxt("freq_3means.dat", means.cluster_centers_)
+np.savetxt("freq_2means_norm.dat", means.cluster_centers_)
 print datetime.now() - startTime
